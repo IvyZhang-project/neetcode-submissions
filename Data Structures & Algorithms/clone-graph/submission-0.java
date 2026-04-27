66947e93f48496463cfdb4b34f8c65c2b7bf9f98
IvyZhang-project <ivyzhangwen@gmail.com>
2026-04-27 13:47:41 -0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,46 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return null;
+        }
+        Node newHead = new Node(node.val);
+        Map<Node, Node> map = new HashMap<>();
+        map.put(node, newHead);
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        while(!queue.isEmpty()){
+            Node cur = queue.poll();
+            List<Node> newNeighbors = new ArrayList<>();
+            for(Node c: cur.neighbors){
+                if(!map.containsKey(c)){
+                    Node nc = new Node(c.val);
+                    queue.add(c);
+                    map.put(c,nc);
+                }
+                newNeighbors.add(map.get(c));
+            }
+            map.get(cur).neighbors = newNeighbors;
+        }
+        return newHead;
+    }
+}
